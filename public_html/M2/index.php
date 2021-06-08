c659f66b7cc3052671051565455daa773ca1e295
nayeem kamal <nhk6@njit.edu>
2021-06-07 22:17:02 -0400

    edited index files

--- public_html/M2/index.php
+++ public_html/M2/index.php
@@ -1,9 +1,8 @@
 <h1>Module 2 Table of Contents</h1>
 <ul>
-    <li><a href="data_types.php">PHP and JS Data Types</a></li>
-    <li><a href="loops.php">PHP and JS Loops</a>
-    <li><a href="floats.php">Important: Dealing with Floats/Decimals</a></li>
-    <li><a href="practice.php">Practice</a></li>
+    <li><a href="problem1.php">Problem 1</a></li>
+    <li><a href="problem2.php">Problem 2</a>
+    <li><a href="problem3.php">Problem 3</a></li>
     <li><a href="..">Back</a></li>
 </ul>
 </ul>
