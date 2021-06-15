7df709de1f4e32007260faf582f568a7bf71f6dd
nayeem kamal <nhk6@njit.edu>
2021-06-14 20:42:58 -0400

    did assignment

--- IT202-Summer2021/public_html/M2/problem1.php
+++ IT202-Summer2021/public_html/M2/problem1.php
@@ -8,6 +8,11 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
+    foreach( $arr as $i){
+        if( $i % 2){
+            echo $i;
+        }
+    }
 
 }
 echo "Problem 1: Odd Output<br>";
--- IT202-Summer2021/public_html/index.php
+++ IT202-Summer2021/public_html/index.php
@@ -1,5 +1,4 @@
-<h1>Matt Toegel's Live Examples for IT202</h1>
-<h4>Note: New platform on Heroku</h4>
+<h1>Nayeem Kamal's Live Examples for IT202</h1>
 <ul>
     <li><a href="M2">Module 2</a></li>
     <li><a href="M3">Module 3</a></li>
