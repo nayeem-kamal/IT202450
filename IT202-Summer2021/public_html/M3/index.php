b579627191b106f05df7bab9b7de5ec41c9002f6
nayeem kamal <nhk6@njit.edu>
2021-06-14 20:46:51 -0400

    index php fix

--- IT202-Summer2021/public_html/M3/index.php
+++ IT202-Summer2021/public_html/M3/index.php
@@ -1,8 +1,8 @@
 <h1>Module 3 Table of Contents</h1>
 <ul>
-    <li><a href="explore.html">HTML Sample</a></li>
-    <li><a href="form.php">Form Sample</a></li>
-    <li><a href="validation.php">Validation Practice</a></li>
+    <li><a href="problem1.html">problem 1</a></li>
+    <!-- <li><a href="form.php">Form Sample</a></li>
+    <li><a href="validation.php">Validation Practice</a></li> -->
     <li><a href="..">Back</a></li>
 </ul>
 </ul>
