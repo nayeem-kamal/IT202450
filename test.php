af1d112740f32a3e67a576d7ab8a2ea77659a343
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-06 18:49:47 -0400

    some db functionality tests

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php
-require_once(__DIR__ . "/../../partials/nav.php");
 if (isset($_POST["submit"])) {
     $email = se($_POST, "email", null, false);
     $password = trim(se($_POST, "password", null, false));
