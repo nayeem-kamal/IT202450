b6e67c49f94602ee17d1c36e95dd0b15d825d32b
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-06 18:00:35 -0400

    some db functionality tests

--- customer_add.php
+++ customer_add.php
@@ -1,6 +1,53 @@
 <?php
 include "header.php";
 include "navbar.php";
+if (isset($_POST["submit"])) {
+    $email = se($_POST, "email", null, false);
+    $password = trim(se($_POST, "password", null, false));
+    $confirm = trim(se($_POST, "confirm", null, false));
+    $username = trim(se($_POST, "username", null, false));
+
+    $isValid = true;
+    if (!isset($email) || !isset($password) || !isset($confirm) || !isset($username)) {
+        flash("Must provide email, username, password, and confirm password","warning");
+        $isValid = false;
+    }
+    if ($password !== $confirm) {
+        flash("Passwords don't match", "warning");
+        $isValid = false;
+    }
+    if (strlen($password) < 3) {
+        flash("Password must be 3 or more characters", "warning");
+        $isValid = false;
+    }
+    $email = sanitize_email($email);
+    if (!is_valid_email($email)) {
+        flash("Invalid email", "warning");
+        $isValid = false;
+    }
+    else{
+        echo $email;
+    }
+    //TODO add validation for username (length? disallow special chars? etc)
+    if ($isValid) {
+        //do our registration
+        $db = getDB();
+        $stmt = $db->prepare("INSERT INTO Users (email, username, password) VALUES (:email, :username, :password)");
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        try {
+            $stmt->execute([":email" => $email, ":password" => $hash, ":username"=>$username]);
+            flash("You've successfully registered, please login");
+            die(header("Location: login.php"));
+        } catch (PDOException $e) {
+            $code = se($e->errorInfo, 0, "00000", false);
+            if ($code === "23000") {
+                flash("An account with this email already exists", "danger");
+            } else {
+                echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +58,7 @@ include "navbar.php";
 </head>
 
 <body>
-    <form class="add_customer_form" action="customer_add_action.php" method="post">
+    <form class="add_customer_form" method="post">
         <div class="flex-container-form_header">
             <h1 id="form_header">Register</h1>
         </div>
@@ -19,7 +66,7 @@ include "navbar.php";
         <div class="flex-container">
             <div class=container>
                 <label>UserName :</label><br>
-                <input name="uname" size="30" type="text" required />
+                <input name="username" size="30" type="text" required />
             </div>
 
         </div>
@@ -39,13 +86,13 @@ include "navbar.php";
         <div class="flex-container">
             <div class=container>
                 <label>Password :</b></label><br>
-                <input name="pwd" size="30" type="text" required />
+                <input name="password" size="30" type="password" required />
             </div>
         </div>
         <div class="flex-container">
             <div class=container>
                 <label>Confirm Password :</b></label><br>
-                <input name="confpwd" size="30" type="text" required />
+                <input name="confirm" size="30" type="password" required />
             </div>
         </div>
 
@@ -66,7 +113,43 @@ include "navbar.php";
         function confirmReset() {
             return confirm('Do you really want to reset?')
         }
-    </script>
+    function validate(form) {
+        let email = form.email.value;
+        let username = form.username.value;
+        let password = form.password.value;
+        let confirm = form.confirm.value;
+        let isValid = true;
+        if (email) {
+            email = email.trim();
+        }
+        if(username)    {
+            username = username.trim();
+        }
+        if (password) {
+            password = password.trim();
+        }
+        if (confirm) {
+            confirm = confirm.trim();
+        }
+        if(!username || username.length === 0)    {
+            isValid    =    false;
+            alert("Must provide a username");
+        }
+        if (email.indexOf("@") === -1) {
+            isValid = false;
+            alert("Invalid email");
+        }
+        if (password !== confirm) {
+            isValid = false;
+            alert("Passwords don't match");
+        }
+        if (password.length < 3) {
+            isValid = false;
+            alert("Password must be 3 or more characters");
+        }
+        return isValid;
+    }
+</script>
 
 </body>
 
---
+++ functions.php
@@ -0,0 +1,71 @@
+<?php
+require_once( "/db.php");
+$BASE_PATH = '/Project/';//This is going to be a helper for redirecting to our base project path since it's nested in another folder
+function se($v, $k = null, $default = "", $isEcho = true) {
+    if (is_array($v) && isset($k) && isset($v[$k])) {
+        $returnValue = $v[$k];
+    } else if (is_object($v) && isset($k) && isset($v->$k)) {
+        $returnValue = $v->$k;
+    } else {
+        $returnValue = $v;
+    }
+    if (!isset($returnValue)) {
+        $returnValue = $default;
+    }
+    if ($isEcho) {
+        //https://www.php.net/manual/en/function.htmlspecialchars.php
+        echo htmlspecialchars($returnValue, ENT_QUOTES);
+    } else {
+        //https://www.php.net/manual/en/function.htmlspecialchars.php
+        return htmlspecialchars($returnValue, ENT_QUOTES);
+    }
+}
+function sanitize_email($email = "") {
+    return filter_var(trim($email), FILTER_SANITIZE_EMAIL);
+}
+function is_valid_email($email = "") {
+    return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
+}
+//User Helpers
+function is_logged_in() {
+    return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
+}
+function has_role($role) {
+    if (is_logged_in() && isset($_SESSION["user"]["roles"])) {
+        foreach ($_SESSION["user"]["roles"] as $r) {
+            if ($r["name"] === $role) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function get_username() {
+    if (is_logged_in()) { //we need to check for login first because "user" key may not exist
+        return se($_SESSION["user"], "username", "", false);
+    }
+    return "";
+}
+function get_user_email() {
+    if (is_logged_in()) { //we need to check for login first because "user" key may not exist
+        return se($_SESSION["user"], "email", "", false);
+    }
+    return "";
+}
+function get_user_id() {
+    if (is_logged_in()) { //we need to check for login first because "user" key may not exist
+        return se($_SESSION["user"], "id", false, false);
+    }
+    return false;
+}
+//flash message system
+function flash($msg = "", $color = "info") {
+    $message = ["text" => $msg, "color" => $color];
+    if (isset($_SESSION['flash'])) {
+        array_push($_SESSION['flash'], $message);
+    } else {
+        $_SESSION['flash'] = array();
+        array_push($_SESSION['flash'], $message);
+    }
+}
+?>
