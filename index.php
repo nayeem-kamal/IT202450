46a5aeb3e9698865c61d7810e4c17d8c098d8c6f
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-07 00:26:53 -0400

    some login tests

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($_POST["submit"])) {
                     //fetch account info, or create an account if the user existed before this feature was added
                     //in my project, a user will have only 1 account associated with them so it's a 1:1 relationship
                     get_or_create_account();//applies directly to the session, make sure it's called after the session is set
-                    die(header("Location: index.php"));
+                    die(header("Location: header.php"));
                 } else {
                     se("Passwords don't match");
                 }
