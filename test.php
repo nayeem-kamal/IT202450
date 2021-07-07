34250fb2a844b27f007122ae9af23deae09ff061
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-06 22:50:54 -0400

    some db functionality tests

--- test.php
+++ test.php
@@ -7,46 +7,15 @@ if (isset($_POST["submit"])) {
     $username = trim(se($_POST, "username", null, false));
 
     $isValid = true;
-    if (!isset($email) || !isset($password) || !isset($confirm) || !isset($username)) {
-        flash("Must provide email, username, password, and confirm password","warning");
-        $isValid = false;
-    }
-    if ($password !== $confirm) {
-        flash("Passwords don't match", "warning");
-        $isValid = false;
-    }
-    if (strlen($password) < 3) {
-        flash("Password must be 3 or more characters", "warning");
-        $isValid = false;
-    }
-    $email = sanitize_email($email);
-    if (!is_valid_email($email)) {
-        flash("Invalid email", "warning");
-        $isValid = false;
-    }
-    //TODO add validation for username (length? disallow special chars? etc)
-    if ($isValid) {
-        //do our registration
-        $db = getDB();
-        $stmt = $db->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        echo($hash);
-        echo($stmt);
-        // try {
-        //     $stmt->execute([":email" => $email, ":password" => $hash, ":username"=>$username]);
-        //     flash("You've successfully registered, please login");
-        //     //die(header("Location: index.php"));
-        // } catch (PDOException $e) {
-        //     $code = se($e->errorInfo, 0, "00000", false);
-        //     if ($code === "23000") {
-        //         flash("An account with this email already exists", "danger");
-        //     } else {
-        //         echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
-        //     }
-        // }
-    }
+
+    echo($email);
+    echo($username);
+    include "flash.php";
 }
 ?>
+
+   
+
 <div>
     <h1>Register</h1>
     <form method="POST" onsubmit="return validate(this);">
@@ -109,6 +78,3 @@ if (isset($_POST["submit"])) {
         return isValid;
     }
 </script>
-<?php
-require_once("flash.php");
-?>
