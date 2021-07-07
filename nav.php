4150b715c868f0b0903a7715488dafd192ea972c
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-06 23:41:43 -0400

    some db functionality tests

--- nav.php
+++ nav.php
@@ -18,6 +18,8 @@ require_once("functions.php");
 
 ?>
 <!-- CSS only -->
+<link rel="stylesheet" href="navbar_style.css">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -29,7 +31,13 @@ require_once("functions.php");
             <h1>NJIT BANK</h1>
         </div>
     </div>
-    <div class="container-fluid">
+    <div class="nav-wrapper">
+    <div class="topnav" id="theTopNav">
+        <a href="./index.php">HOME</a>
+        
+    </div>
+    </div>
+    <!-- <div class="container-fluid">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <?php if (is_logged_in()) : ?>
                 <li class="nav-item"><a class="nav-link" href="home.php">Home</a></li>
@@ -54,5 +62,5 @@ require_once("functions.php");
                 <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
             <?php endif; ?>
         </ul>
-    </div>
+    </div> -->
 </nav>
