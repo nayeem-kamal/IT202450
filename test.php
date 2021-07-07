f73a564245c725e5581974f98f4462ec5c868aab
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-06 23:09:57 -0400

    some db functionality tests

--- test.php
+++ test.php
@@ -25,13 +25,24 @@ if (isset($_POST["submit"])) {
         $isValid = false;
     }
 
-    if($isValid){
-
- ?>
- <h1>success</h1>
- <?php
-   
-}
+    if ($isValid) {
+        //do our registration
+        $db = getDB();
+        $stmt = $db->prepare("INSERT INTO users (email, username, password) VALUES (:email, :username, :password)");
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        try {
+            $stmt->execute([":email" => $email, ":password" => $hash, ":username"=>$username]);
+            flash("You've successfully registered, please login");
+            die(header("Location: index.php"));
+        } catch (PDOException $e) {
+            $code = se($e->errorInfo, 0, "00000", false);
+            if ($code === "23000") {
+                flash("An account with this email already exists", "danger");
+            } else {
+                echo "<pre>" . var_export($e->errorInfo, true) . "</pre>";
+            }
+        }
+    }
 }
 ?>
 
