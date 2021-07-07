e3a7ab49d805c951ee8be591bb60658ba8f1aa6b
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-07 00:47:51 -0400

    some login tests

--- home.php
+++ home.php
@@ -1,11 +1,15 @@
 <?php
 require_once("nav.php");
 if (!is_logged_in()) {
-    die(header("Location: index.php"));
-}
+    //die(header("Location: index.php"));
+    ?>
+    <h1>failed login</h1>0
+    <?php
+}else{
 ?>
 <h1>Home</h1>
 <h5>Welcome, <?php se(get_username()); ?>!</h5>
 <?php
+}
 require_once("flash.php");
 ?>
