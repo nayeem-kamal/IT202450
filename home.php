9f6e3187f1954fb97c59340b4ae7b10a44543037
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-07 01:43:26 -0400

    some login tests

--- home.php
+++ home.php
@@ -3,7 +3,8 @@ require_once("nav.php");
 if (!is_logged_in()) {
     //die(header("Location: index.php"));
     ?>
-        <h1><?php echo($_SESSION["user"]["username"]);?></h1>
+        <h1><?php echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+?></h1>
     <?php
 }else{
 ?>
