cd1694572cffd8efac43db3c884440b4e832c625
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-11 20:00:34 -0400

    Testing Accounts

--- add_account.php
+++ add_account.php
@@ -7,6 +7,37 @@ if (!is_logged_in()) {
     die(header("Location: index.php"));
     flash("Cannot access this page without logging in", "warning");
 }
+
+if (isset($_POST["submit"])) {
+    $type = se($_POST, "accountType", null, false);
+    $created = false;
+    $query = "INSERT INTO Accounts (account_number, user_id, account_type) VALUES (:an, :uid, :at)";
+    $stmt = $db->prepare($query);
+    $user_id = get_user_id();
+    $account_number = "";
+    
+        while (!$created) {
+        try {
+            $account_number = get_random_str(12);
+            $stmt->execute([":an" => $account_number, ":uid" => $user_id, ":at" => $type]);
+            $created = true; //if we got here it was a success, let's exit
+            flash("Your account has been created successfully", "success");
+        } catch (PDOException $e) {
+            $code = se($e->errorInfo, 0, "00000", false);
+            if (
+                $code !== "23000"
+            ) {
+                flash("Error: We are unable to create or access your account at this time", "danger");
+            
+        }
+            }
+        }
+    }
+
+
+
+
+
 ?>
 
 <head>
@@ -27,7 +58,7 @@ if (!is_logged_in()) {
             </div>
             <div class="flex-container">
                 <div class=container>
-                    <label for="email">Email: </label>
+                    <label for="email">Account Type: </label>
                     <input list="AccountType" id="accountType" name="accountType" required />
                     <datalist id="AccountType">
                         <option value="Checking">
