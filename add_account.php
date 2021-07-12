a4d90bf9cb5aa4630b4b45f07db14d547f93cccd
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-11 20:15:33 -0400

    Testing Accounts

--- add_account.php
+++ add_account.php
@@ -42,8 +42,11 @@ if (isset($_POST["submit"])) {
 ?>
 
 <head>
-   
-   <link rel="stylesheet" href="customer_add_style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="customer_add_style.css">
 </head>
 
 
@@ -51,9 +54,11 @@ if (isset($_POST["submit"])) {
     <div>
 
         <form method="POST">
-            <div class="flex-container-form_header">
+            <!-- <div class="flex-container-form_header">
                 <h1 id="form_header">Create an account</h1>
-            </div>
+            </div> -->
+            <h1 class="display-4">Create an Account</h1>
+
             <div class="flex-container">
                 <div class=container>
                     <label for="email">Account Type: </label>
