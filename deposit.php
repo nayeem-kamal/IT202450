36a312f85769cffd5db4b5c2e35d77b1f395e8a0
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-07-12 22:39:04 -0400

    Transaction history

--- deposit.php
+++ deposit.php
@@ -9,7 +9,7 @@ if (!is_logged_in()) {
 } else {
 
     $db = getDB();
-    $query = "SELECT account_number from Accounts where user_id = :uid LIMIT 5";
+    $query = "SELECT * from Accounts where user_id = :uid LIMIT 5";
 
     $created = false;
     $stmt = $db->prepare($query);
