1dc977202a373a66e67dbfe2a75f02585df617e3
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-08-02 09:39:49 -0400

    taking out loans

--- pay_loan.php
+++ pay_loan.php
@@ -44,7 +44,7 @@ if (!is_logged_in()) {
                             flash("Error: We are unable to create or access your account at this time" . $e, "danger");
                         }
                             ?>
-                </datalist>
+                                   </datalist>
             </div>
         </div>
         <div class="flex-container">
@@ -112,6 +112,8 @@ if (!is_logged_in()) {
 
                             if (transaction($src, $destination, $amount, "Loan Payment", $memo)) {
                                 flash("Your transfer has been created successfully", "success");
+                                $URL="https://nhk6-dev.herokuapp.com/dashboard.php";
+                                echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
                                 
                             } else {
                                 flash("Your transfer did not complete. Possibly insufficient funds.", "danger");
@@ -123,8 +125,6 @@ if (!is_logged_in()) {
                 }
 
 ?>
-
-
 <head>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
